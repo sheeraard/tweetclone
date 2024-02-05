6284f2fc0cdec8f41312b82880fb32fbe4be5f3b
sheeraard <128192460+sheeraard@users.noreply.github.com>
2024-02-05 17:24:17 +0700

    Add files via upload

---
+++ app/Controllers/Tweet.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Controllers;
+
+use \App\Models\TweetModel;
+
+class Tweet extends BaseController
+{
+    
+    var $categories;
+    var $sess;
+    var $curUser;
+
+    var $tweetMdl;
+		var $profile;
+
+    public function __construct()
+    {
+        $this->categories = (new \Config\AdtConfig())->getCategories();
+        $this->sess = session();
+        $this->curUser = $this->sess->get('currentuser');
+
+        $this->tweetMdl = new TweetModel();
+				$userMdl = new \App\Models\UserModel();
+        $this->profile = $userMdl->find($this->curUser['userid']);
+    }
+    public function index()
+    {       
+        $data['categories'] = $this->categories;
+        $data['judul'] = 'Tweet Terbaru';
+
+        $data['profile'] = $this->profile;
+        $data['tweets'] = $this->tweetMdl->getLatest();
+
+        return view('tweet_home', $data);
+    }
+
+    public function category($category)
+    {
+        $data['categories'] = $this->categories;
+        $data['judul'] = 'Tweet Terbaru';
+
+        $data['profile'] = $this->profile;
+        $data['tweets'] = $this->tweetMdl->getByCategory($category);
+
+        return view('tweet_home', $data);
+    }
+
+    public function addForm()
+    {
+        $data['categories'] = $this->categories;
+        return view('tweet_add', $data);
+    }
+
+    public function editForm($tweet_id)
+    {
+        $tweet = $this->tweetMdl->find($tweet_id);
+        if($tweet->user_id != $this->sess->get('currentuser')['userid']){
+            $this->sess->set('edittweet', 'error');
+            return redirect()->to('/');
+        }
+        
+        $data['categories'] = $this->categories;
+        $data['tweet'] = $tweet;
+        return view('tweet_edit', $data);
+    }
+
+    public function addTweet()
+    {
+        $this->tweetMdl->newTweet($this->sess->get('currentuser'), $this->request->getPost());
+        $this->sess->setFlashdata('addtweet', 'success');
+        return redirect()->to('/');
+    }
+
+    public function delTweet($tweet_id)
+    {
+        $result = $this->tweetMdl->delTweet($this->sess->get('currentuser')['userid'], $tweet_id);
+        if($result){
+            $this->sess->setFlashdata('deltweet', 'success');
+        } else {
+            $this->sess->setFlashdata('deltweet', 'error');
+        }
+        return redirect()->to('/');
+    }
+
+    public function editTweet()
+    {
+        $result = $this->tweetMdl->editTweet($this->request->getPost());
+        if($result){
+            $this->sess->setFlashdata('edittweet', 'success');
+        } else {
+            $this->sess->setFlashdata('edittweet', 'error');
+        }
+
+        return redirect()->to('/');
+    }
+
+}
---
+++ app/Database/Migrations/2024-02-05-092239_Createusers.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+
+class Createusers extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField(
+            [
+                'id',
+                'username' => [
+                    'type' => 'VARCHAR',
+                    'constraint' => '100',
+                    'unique' => true,
+                ],
+                'password' => [
+                    'type' =>  'VARCHAR',
+                    'constraint' => '100',
+                ],
+                'fullname' => [
+                    'type' => 'VARCHAR',
+                    'constraint' => '100',
+                ],
+            ]
+        );
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users', true);
+    }
+}
